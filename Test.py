1c102eb6c8e1568192b6693b3f57b6d338265eac
Jett <74122461+JettGazeley@users.noreply.github.com>
2021-12-28 12:56:58 -0500

    had chat with Kaam to discuss next steps. Will be making new file to test Excel input

--- Test.py
+++ Test.py
@@ -3,22 +3,35 @@
 
 
 #sample data
+input_data = []
 group_names = ["name1", "name2", "name3"]
 group_found = ["name1", "2", "4"]
-group_missing = ["1", "5", "3"]
+group_missing = ["1", 5, "3"]
 
 #check if data is found in the sample
 def data_check():
     for i in range (0, len(group_names)):
         if group_names[i] in group_found:
             print("found a matched data point")
+            print(group_names[i])
         if group_names[i] not in group_missing:
             print("missing a data point") 
     return 0
 
+def input_check():
+    sample = int(input("Enter your data: "))
+    if sample in group_missing:
+        print("hey - we found it")
+        #found_important_data()
+    return 0
+
+
+
+
 #main function
 def main():
     data_check()
+    input_check()
 
 if __name__ == "__main__":
     main()
