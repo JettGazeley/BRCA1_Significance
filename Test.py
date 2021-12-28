615a7088edb5c38194a6b36b3b7f02dad3900b98
Jett <74122461+JettGazeley@users.noreply.github.com>
2021-12-27 22:35:19 -0500

    Created data_check function for simple data

--- Test.py
+++ Test.py
@@ -1,22 +1,19 @@
+#sample data
 group_names = ["name1", "name2", "name3"]
-group_found = []
-group_missing = []
+group_found = ["name1", "2", "4"]
+group_missing = ["1", "5", "3"]
 
-
-def data ():
+#check if data is found in the sample
+def data_check():
     for i in range (0, len(group_names)):
         if group_names[i] in group_found:
             print("found a matched data point")
-
+        if group_names[i] not in group_missing:
+            print("missing a data point") 
     return 0
 
-
-
-
-
-
 def main():
-    print("Hello World!")
+    data_check()
 
 if __name__ == "__main__":
     main()
